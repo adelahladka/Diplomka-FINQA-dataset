64ec294afc352ddec45923e7f1863bace2681844
DESKTOP-6VS722T\Uzivatel <benesova.work@gmail.com>
2024-07-20 14:47:40 +0200

    update: json-csv script added

---
+++ AB-code/initial-exploration.R
@@ -0,0 +1,5 @@
+#loading the data
+
+data1 <- read.csv("C:\\Users\\Uzivatel\\Documents\\Diplomka-FINQA-dataset\\AB-code\\output.csv", header=TRUE, stringsAsFactors=FALSE)
+View(data1)
+summary(data1)
